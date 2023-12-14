b99abeeb07f4cf70b88d7cb5e541ee2a0974b111
guycastle <guillaume.vandecasteele@2invision.eu>
2023-12-14 14:43:30 +0100

    Day 10 part 2

--- src/main/scala/days/day10/DayTen.scala
+++ src/main/scala/days/day10/DayTen.scala
@@ -1,110 +1,145 @@
 package days.day10
 
 import days.DailyChallenge
-import days.day10.DayTen.Direction.West
-import days.day10.DayTen.TileType.Animal
 
-import scala.annotation.{tailrec, targetName}
+import scala.annotation.tailrec
 
 object DayTen extends DailyChallenge[Int]:
 
   override lazy val day: Int = 10
 
   override def partOne(input: Seq[String]): Int =
-    val (animal, maze) = parseInput(input)
-    mazeHalfwayPoint(animalTile = animal, maze = maze)
+    val terrain    = parseInput(input)
+    val loopLength = terrain.map(_.count(_.loop)).sum
+    if loopLength % 2 == 0 then loopLength / 2 else (loopLength / 2)
 
-  override def partTwo(input: Seq[String]): Int = 0
+  override def partTwo(input: Seq[String]): Int =
+    val terrain = parseInput(input)
+    terrain.foldLeft(0):
+      case (enclosed, row) => countEnclosed(row, enclosed)
+  end partTwo
 
   @main def run(): Unit = evaluate()
 
-  def mazeHalfwayPoint(animalTile: Tile, maze: Maze): Int =
-    @tailrec
-    def go(tile: Tile, origin: Option[Coordinates] = None, steps: Int = 0): Int = origin match
-      case Some(o) => (for
-          newDirection <- tile.tileType.ports.find(dir => dir.goFrom(tile.coordinates) != o)
-          nextTile     <- maze.get(newDirection.goFrom(tile.coordinates))
-        yield nextTile) match
-          case Some(next) if next.tileType == Animal => steps + 1
-          case Some(next)                            => go(next, Some(tile.coordinates), steps = steps + 1)
-          case _                                     => throw new IllegalArgumentException(s"Reached dead end at ${tile.coordinates}")
-      case None    =>
-        if tile.tileType != TileType.Animal then throw new IllegalArgumentException("First tile must be animal!")
-        else
-          maze.getConnectedPipes(tile.coordinates).headOption match
-            case None           => steps
-            case Some(nextTile) => go(tile = nextTile, origin = Some(tile.coordinates), steps = steps + 1)
-    val loop = go(tile = animalTile)
-    if loop % 2 != 0 then (loop / 2) + 1 else loop / 2
-
-  type Row  = Seq[Tile]
-  type Maze = Seq[Row]
-  extension (maze: Maze)
-    def get:               Coordinates => Option[Tile] = (x, y) =>
-      if x < 0 || y < 0 then None
-      else
-        for
-          row  <- maze.lift(y)
-          tile <- row.lift(x)
-        yield tile
-    def getConnectedPipes: Coordinates => Seq[Tile]    = coord =>
-      maze.get(coord) match
-        case Some(tile) if tile.tileType == TileType.Ground => Seq.empty
-        case Some(tile) if tile.tileType == TileType.Animal =>
-          Direction.values
-            .flatMap(dir => maze.get(dir.goFrom(tile.coordinates)))
-            .find(_.connections.contains(tile.coordinates))
-            .toSeq
-        case Some(tile)                                     => tile.tileType.ports.flatMap(dir => maze.get(dir.goFrom(tile.coordinates)))
-        case None                                           => Seq.empty
-  end extension
-  object Maze:
-    def empty: Maze = Seq.empty
-  end Maze
-
-  type Coordinates = (Int, Int)
-  extension (c: Coordinates)
-    @targetName("plus")
-    def + : Coordinates => Coordinates = (a, b) => (c._1 + a, c._2 + b)
-  end extension
-
-  enum Direction(val goFrom: Coordinates => Coordinates):
-    case North     extends Direction(goFrom = _ + (0, -1))
-    case NorthWest extends Direction(goFrom = _ + (-1, -1))
-    case NorthEast extends Direction(goFrom = _ + (1, -1))
-    case West      extends Direction(goFrom = _ + (-1, 0))
-    case East      extends Direction(goFrom = _ + (1, 0))
-    case South     extends Direction(goFrom = _ + (0, 1))
-    case SouthWest extends Direction(goFrom = _ + (-1, 1))
-    case SouthEast extends Direction(goFrom = _ + (1, 1))
+  enum Direction:
+    case Up, Down, Left, Right
   end Direction
+  extension (d: Direction)
+    def opposite: Direction                  = d match
+      case Direction.Up    => Direction.Down
+      case Direction.Down  => Direction.Up
+      case Direction.Right => Direction.Left
+      case Direction.Left  => Direction.Right
+    def move:     Coordinates => Coordinates = c =>
+      d match
+        case Direction.Up    => c.copy(y = c.y - 1)
+        case Direction.Down  => c.copy(y = c.y + 1)
+        case Direction.Left  => c.copy(x = c.x - 1)
+        case Direction.Right => c.copy(x = c.x + 1)
+  end extension
 
-  lazy val parseInput: Seq[String] => (Tile, Maze) = input =>
-    val (start, maze) = input.zipWithIndex.foldLeft((Option.empty[Tile], Maze.empty)):
-      case ((animal, maze), (line, y)) =>
-        val tiles       = line.zipWithIndex.map((char, x) =>
-          Tile(
-            coordinates = (x, y),
-            tileType    = TileType.values
-              .find(_.char == char)
-              .getOrElse(throw new IllegalArgumentException(s"Invalid tile character: $char"))
-          )
+  case class Coordinates(x: Int, y: Int)
+
+  case class Tile(coords: Coordinates, tileType: TileType, loop: Boolean = false)
+
+  type Row     = Seq[Tile]
+  type Terrain = Seq[Row]
+  extension (t: Terrain)
+    def get(coords: Coordinates): Option[Tile] = t.lift(coords.y).flatMap(_.lift(coords.x))
+    def update(tile: Tile): Terrain = (for
+      row <- t.lift(tile.coords.y)
+      _   <- row.lift(tile.coords.x)
+    yield t.updated(tile.coords.y, row.updated(tile.coords.x, tile))).getOrElse(t)
+    // only works on a freshly parsed input with one tile marked as part of loop
+    def start:              Tile    = t
+      .flatMap(_.find(_.loop == true))
+      .headOption
+      .map(start => {
+        val startPorts = Seq(
+          t.get(Direction.Up.move(start.coords)).filter(_.tileType.ports.contains(Direction.Down)).map(_ => Direction.Up),
+          t.get(Direction.Down.move(start.coords))
+            .filter(_.tileType.ports.contains(Direction.Up))
+            .map(_ => Direction.Down),
+          t.get(Direction.Right.move(start.coords))
+            .filter(_.tileType.ports.contains(Direction.Left))
+            .map(_ => Direction.Right),
+          t.get(Direction.Left.move(start.coords))
+            .filter(_.tileType.ports.contains(Direction.Right))
+            .map(_ => Direction.Left)
+        ).flatten.sortBy(_.ordinal)
+        start.copy(
+          tileType = TileType.values
+            .find(_.ports.sortBy(_.ordinal) == startPorts)
+            .getOrElse(
+              throw new IllegalArgumentException(
+                s"Could not determine start tile (${start.coords.x}, ${start.coords.y}) type!"
+              )
+            )
         )
-        val updatedMaze = maze :+ tiles
-        (animal.orElse(tiles.find(_.tileType == Animal)), updatedMaze)
-    (start.getOrElse(throw new IllegalArgumentException("No animal found!")), maze)
+      })
+      .getOrElse(throw new IllegalArgumentException("No starting point found!"))
+  end extension
 
-  case class Tile(coordinates: Coordinates, tileType: TileType):
-    def connections: Seq[Coordinates] = this.tileType.ports.map(_ goFrom this.coordinates)
+  lazy val parseInput: Seq[String] => Terrain = input =>
+    val terrain = input.zipWithIndex.foldLeft(Seq.empty[Seq[Tile]]):
+      case (map, (line, y)) => map :+ line.zipWithIndex.map: (char, x) =>
+             val tileType = TileType.forChar(char)
+             Tile(coords = Coordinates(x = x, y = y), loop = tileType == TileType.Animal, tileType = tileType)
+    mapLoop(terrain)
+  end parseInput
+
+  @tailrec
+  private def mapLoop(terrain: Terrain, tile: Option[Tile] = None, origin: Option[Direction] = None): Terrain =
+    (tile, origin) match
+      case (Some(current), _) if current.loop => terrain // we're back to our starting point
+      case (Some(current), Some(from))        =>
+        val direction = current.tileType.ports
+          .filterNot(_ == from)
+          .headOption
+          .getOrElse(throw new IllegalArgumentException("Got nowhere to go!"))
+        mapLoop(
+          terrain = terrain.update(current.copy(loop = true)),
+          tile    = terrain.get(direction.move(current.coords)),
+          origin  = Some(direction.opposite)
+        )
+      case _                                  =>
+        val startingPoint = terrain.start
+        val direction     =
+          startingPoint.tileType.ports.headOption.getOrElse(throw new IllegalArgumentException("Got nowhere to go!"))
+        mapLoop(
+          terrain = terrain.update(startingPoint),
+          tile    = terrain.get(direction.move(startingPoint.coords)),
+          origin  = Some(direction.opposite)
+        )
+  end mapLoop
+
+  @tailrec
+  private def countEnclosed(row: Row, enclosed: Int, processed: Row = Seq.empty): Int =
+    if row.size <= 2 || !row.exists(_.loop) then enclosed
+    else
+      lazy val isSouthBoundLoop: Tile => Boolean = tile => tile.loop && TileType.southBound.contains(tile.tileType)
+      val (skip, toCheck)         = row.span(!isSouthBoundLoop(_))
+      val updatedProcessed        = (processed ++ skip) ++ toCheck.headOption
+      val (enclosedPart, remnant) = toCheck.drop(1).span(!isSouthBoundLoop(_))
+      val enclosedCount           = if (updatedProcessed.count(isSouthBoundLoop) % 2 != 0) enclosedPart.count(!_.loop) else 0
+      countEnclosed(remnant, enclosedCount + enclosed, updatedProcessed)
+  end countEnclosed
 
   enum TileType(val char: Char, val ports: Seq[Direction]):
-    case Vertical   extends TileType(char = '|', ports = Seq(Direction.North, Direction.South))
-    case Horizontal extends TileType(char = '-', ports = Seq(Direction.West, Direction.East))
-    case NorthEast  extends TileType(char = 'L', ports = Seq(Direction.North, Direction.East))
-    case NorthWest  extends TileType(char = 'J', ports = Seq(Direction.North, West))
-    case SouthWest  extends TileType(char = '7', ports = Seq(Direction.West, Direction.South))
-    case SouthEast  extends TileType(char = 'F', ports = Seq(Direction.East, Direction.South))
+    case Vertical   extends TileType(char = '|', ports = Seq(Direction.Up, Direction.Down))
+    case Horizontal extends TileType(char = '-', ports = Seq(Direction.Left, Direction.Right))
+    case UpRight    extends TileType(char = 'L', ports = Seq(Direction.Up, Direction.Right))
+    case UpLeft     extends TileType(char = 'J', ports = Seq(Direction.Up, Direction.Left))
+    case DownLeft   extends TileType(char = '7', ports = Seq(Direction.Down, Direction.Left))
+    case DownRight  extends TileType(char = 'F', ports = Seq(Direction.Down, Direction.Right))
     case Ground     extends TileType(char = '.', ports = Seq.empty)
     case Animal     extends TileType(char = 'S', ports = Seq.empty)
+  end TileType
+  object TileType:
+    def forChar(char: Char): TileType      = TileType.values
+      .find(_.char == char)
+      .getOrElse(throw new IllegalArgumentException(s"Invalid tile character '$char'"))
+    lazy val southBound:     Seq[TileType] = Seq(Vertical, DownLeft, DownRight)
+    lazy val northBound:     Seq[TileType] = Seq(Vertical, UpRight, UpLeft)
   end TileType
 end DayTen
--- src/test/scala/days/day10/DayTenTest.scala
+++ src/test/scala/days/day10/DayTenTest.scala
@@ -1,7 +1,6 @@
 package days.day10
 
 import days.BaseTest
-import days.day10.DayTen.TileType
 
 class DayTenTest extends BaseTest:
 
@@ -21,18 +20,53 @@ class DayTenTest extends BaseTest:
     "LJ.LJ"
   )
 
-  "Day ten's solution" must:
+  lazy val test3: Seq[String] = Seq(
+    "...........",
+    ".S-------7.",
+    ".|F-----7|.",
+    ".||.....||.",
+    ".||.....||.",
+    ".|L-7.F-J|.",
+    ".|..|.|..|.",
+    ".L--J.L--J.",
+    "..........."
+  )
 
-    "correctly parse the maze and find the animal" in:
-      val (animalTile1, maze1) = DayTen.parseInput(test1)
-      val (animalTile2, maze2) = DayTen.parseInput(test2)
-      animalTile1.coordinates mustBe (1, 1)
-      animalTile2.coordinates mustBe (0, 2)
-      maze1.get(animalTile1.coordinates).value.tileType mustBe TileType.Animal
-      maze2.get(animalTile2.coordinates).value.tileType mustBe TileType.Animal
+  lazy val test4: Seq[String] = Seq(
+    ".F----7F7F7F7F-7....",
+    ".|F--7||||||||FJ....",
+    ".||.FJ||||||||L7....",
+    "FJL7L7LJLJ||LJ.L-7..",
+    "L--J.L7...LJS7F-7L7.",
+    "....F-J..F7FJ|L7L7L7",
+    "....L7.F7||L7|.L7L7|",
+    ".....|FJLJ|FJ|F7|.LJ",
+    "....FJL-7.||.||||...",
+    "....L---J.LJ.LJLJ..."
+  )
+
+  lazy val test5: Seq[String] = Seq(
+    "FF7FSF7F7F7F7F7F---7",
+    "L|LJ||||||||||||F--J",
+    "FL-7LJLJ||||||LJL-77",
+    "F--JF--7||LJLJ7F7FJ-",
+    "L---JF-JLJ.||-FJLJJ7",
+    "|F|F-JF---7F7-L7L|7|",
+    "|FFJF7L7F-JF7|JL---7",
+    "7-L-JL7||F7|L7F-7F7|",
+    "L.L7LFJ|||||FJL7||LJ",
+    "L7JLJL-JLJLJL--JLJ.L"
+  )
+
+  "Day ten's solution" must:
 
     "correctly calculate the number of steps required to reach halfway across the pipe loop" in:
       DayTen.partOne(test1) mustBe 4
       DayTen.partOne(test2) mustBe 8
+
+    "correctly calculate the space enclosed in the loop" in:
+      DayTen.partTwo(test3) mustBe 4
+      DayTen.partTwo(test4) mustBe 8
+      DayTen.partTwo(test5) mustBe 10
 
 end DayTenTest
