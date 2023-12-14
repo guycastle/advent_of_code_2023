d74a37a4fcd4382e7f58bbd12863b10d6e56c660
guycastle <guillaume.vandecasteele@2invision.eu>
2023-12-14 22:35:49 +0100

    day eleven part two

--- src/main/scala/days/day11/DayEleven.scala
+++ src/main/scala/days/day11/DayEleven.scala
@@ -1,63 +1,58 @@
 package days.day11
 
 import days.DailyChallenge
-import days.day11.DayEleven.Space.Empty
+
+import scala.annotation.tailrec
 import scala.math.abs
 
-object DayEleven extends DailyChallenge[Int]:
+object DayEleven extends DailyChallenge[Long]:
   override lazy val day: Int = 11
 
-  override def partOne(input: Seq[String]): Int =
-    parseInput(input).map(distanceBetweenGalaxies).sum
+  override def partOne(input: Seq[String]): Long =
+    parseInput(input = input, expandBy = 1).map(distanceBetweenGalaxies).sum
 
-  override def partTwo(input: Seq[String]): Int = 0
+  override def partTwo(input: Seq[String]): Long =
+    parseInput(input = input, expandBy = 999999).map(distanceBetweenGalaxies).sum
 
   @main def run(): Unit = evaluate()
 
-  case class Galaxy(x: Int, y: Int)
+  case class Galaxy(x: Long, y: Long)
 
-  type Row = Seq[Space]
-  type Universe = Seq[Row]
+  type Row        = Seq[Galaxy]
+  type Universe   = Seq[Row]
   type GalaxyPair = (Galaxy, Galaxy)
 
-  enum Space:
-    case Galaxy, Empty
-  end Space
-
-  lazy val distanceBetweenGalaxies: GalaxyPair => Int =
+  lazy val distanceBetweenGalaxies: GalaxyPair => Long =
     case (Galaxy(x1, y1), Galaxy(x2, y2)) => abs(x1 - x2) + abs(y1 - y2)
 
-
-  lazy val parseInput: Seq[String] =>  Seq[GalaxyPair] = input =>
-    val unexpanded = input.map(_.map:
-      case '#' => Space.Galaxy
-      case _ => Space.Empty
-    )
-    val galaxies = unexpanded.headOption match
+  def parseInput(input: Seq[String], expandBy: Int): Seq[GalaxyPair] =
+    val universe: Universe = input.headOption match
       case Some(row) =>
-        val verticalEmptySpaceIndices = row.indices.filter(i => !unexpanded.exists(_.lift(i).contains(Space.Galaxy)))
-        val expanded = unexpanded.flatMap:
-          row =>
-            val expandedRow = verticalEmptySpaceIndices.foldRight(row):
-              case (idx, row) =>
-                val (before, after) = row.splitAt(idx)
-                (before :+ Empty) ++ after
-            if row.forall(_ == Space.Empty) then Seq(expandedRow, expandedRow) else Seq(expandedRow)
-        expanded.zipWithIndex.flatMap:
-          case (row, y) => row.zipWithIndex.collect:
-            case (space, x) if space == Space.Galaxy => Galaxy(x, y)
+        val verticalEmptySpaceIndices = row.indices.filter(i => !input.exists(_.lift(i).contains('#')))
+        val (expanded, _)             = input.zipWithIndex.foldLeft(Seq.empty[Row], 0L):
+          case ((rows, yOffset), (row, y)) =>
+            val newYOffset               = if row.forall(_ == '.') then yOffset + expandBy else yOffset
+            val (expandedRowGalaxies, _) = row.zipWithIndex.foldLeft(Seq.empty[Galaxy], 0L):
+              case ((galaxies, xOffset), (space, x)) =>
+                if verticalEmptySpaceIndices contains x then (galaxies, xOffset + expandBy)
+                else
+                  space match
+                    case '#' => (galaxies :+ Galaxy(x = x + xOffset, y = y + newYOffset), xOffset)
+                    case '.' => (galaxies, xOffset)
+            (rows :+ expandedRowGalaxies, newYOffset)
+        expanded
+      case None      => throw new IllegalArgumentException("Empty universe")
 
-      case None => throw new IllegalArgumentException("Empty universe")
+    @tailrec
+    def pair(galaxies: Seq[Galaxy], pairs: Seq[GalaxyPair] = Seq.empty): Seq[GalaxyPair] = galaxies.headOption match
+      case Some(galaxy) if galaxies.size >= 2 =>
+        val newPairs = galaxies
+          .drop(1)
+          .map: other =>
+            (galaxy, other)
+        pair(galaxies = galaxies.drop(1), pairs = pairs ++ newPairs)
+      case _                                  => pairs
 
-    def pair(galaxies: Seq[Galaxy], pairs: Seq[GalaxyPair] = Seq.empty): Seq[GalaxyPair] =
-      galaxies.headOption match
-        case Some(galaxy) if galaxies.size >= 2 =>
-          val newPairs = galaxies.drop(1).map:
-              other => (galaxy, other)
-          pair(galaxies = galaxies.drop(1), pairs = pairs ++ newPairs)
-        case _ => pairs
-
-    pair(galaxies)
-
+    pair(universe.flatten)
 
 end DayEleven
--- src/test/scala/days/day11/DayElevenTest.scala
+++ src/test/scala/days/day11/DayElevenTest.scala
@@ -20,8 +20,12 @@ class DayElevenTest extends BaseTest:
   "Day eleven's solution" must:
 
     "correctly parse the input and pair galaxies" in:
-      val pairs = DayEleven.parseInput(test)
+      val pairs = DayEleven.parseInput(test, 1)
       pairs.size mustBe 36
 
-    "correctly calculate the total distance between each pair" in:
+    "correctly calculate the total distance between each pair in part one" in:
       DayEleven.partOne(test) mustBe 374
+
+    "correctly calculate the total distance between each pair in part two" in:
+      DayEleven.parseInput(test, 9).map(DayEleven.distanceBetweenGalaxies).sum mustBe 1030
+      DayEleven.parseInput(test, 99).map(DayEleven.distanceBetweenGalaxies).sum mustBe 8410
