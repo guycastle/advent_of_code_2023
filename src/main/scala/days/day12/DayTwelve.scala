ffd3366b8bdf9a31ee29619775f3fc9308253dcd
guycastle <guillaume.vandecasteele@2invision.eu>
2023-12-15 11:52:50 +0100

    day twelve prep

--- src/main/scala/days/day12/DayTwelve.scala
+++ src/main/scala/days/day12/DayTwelve.scala
@@ -2,16 +2,16 @@ package days.day12
 
 import days.DailyChallenge
 
-object DayTwelve extends DailyChallenge[Int]:
+object DayTwelve extends DailyChallenge[Long]:
 
   override lazy val day: Int = 12
 
-  override def partOne(input: Seq[String]): Int = 0
+  override def partOne(input: Seq[String]): Long = 0
 
-  override def partTwo(input: Seq[String]): Int = 0
+  override def partTwo(input: Seq[String]): Long = 0
 
-  @main def run() = evaluate()
-  
+  @main def run(): Unit = evaluate()
+
   enum Status:
     case Operational, Damaged, Unknown
   end Status
@@ -21,11 +21,15 @@ object DayTwelve extends DailyChallenge[Int]:
   lazy val parseInputLine: String => Record = line =>
     val (statuses, cont) = line.span(!_.isWhitespace)
     Record(
-      statuses = statuses.map:
+      statuses          = statuses.map:
         case '?' => Status.Unknown
         case '#' => Status.Damaged
         case '.' => Status.Operational
-      , contiguousDamaged = cont.drop(1).withFilter(_ != ',').map(_.toString.toInt)
+      ,
+      contiguousDamaged = cont.drop(1).withFilter(_ != ',').map(_.toString.toInt)
     )
+
+  //TODO solve this
+  def countArrangements(record: Record, count: Long = 0L): Long = ???
 
 end DayTwelve
--- src/test/scala/days/day12/DayTwelveTest.scala
+++ src/test/scala/days/day12/DayTwelveTest.scala
@@ -14,12 +14,15 @@ class DayTwelveTest extends BaseTest:
     "????.######..#####. 1,6,5",
     "?###???????? 3,2,1"
   )
-  
+
   "Day twelve's solution" must:
 
     "parse the input correctly" in:
-      test.headOption.map(DayTwelve.parseInputLine).value mustBe Record(statuses = Seq(Unknown, Unknown, Unknown, Operational, Damaged, Damaged, Damaged), contiguousDamaged = Seq(1, 1, 3))
-      
+      test.headOption.map(DayTwelve.parseInputLine).value mustBe Record(
+        statuses          = Seq(Unknown, Unknown, Unknown, Operational, Damaged, Damaged, Damaged),
+        contiguousDamaged = Seq(1, 1, 3)
+      )
+
     "calculate the correct sum of possible arrangements" in:
       DayTwelve.partOne(test) mustBe 2
 
