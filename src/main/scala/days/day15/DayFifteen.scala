3732ba5b20cb265f69ab47c878cfc003cd9481b8
guycastle <guillaume.vandecasteele@2invision.eu>
2024-01-19 12:45:06 +0100

    Day 15 part 2

--- src/main/scala/days/day15/DayFifteen.scala
+++ src/main/scala/days/day15/DayFifteen.scala
@@ -2,21 +2,49 @@ package days.day15
 
 import days.DailyChallenge
 
+import scala.collection.immutable.VectorMap
+
 object DayFifteen extends DailyChallenge[Int]:
 
   override lazy val day: Int = 15
 
   override def partOne(input: Seq[String]): Int = parseInput(input).map(_.aocHash).sum
 
-  override def partTwo(input: Seq[String]): Int = 0
+  override def partTwo(input: Seq[String]): Int = parseInput(input)
+    .foldLeft(Map.empty[Int, Box]):
+      case (boxes, op) =>
+        val step = op.toStep
+        boxes.updated(
+          step.box,
+          step.focalLength match
+            case Some(lens) => boxes.getOrElse(step.box, Box(step.box)).updated(step.label, lens)
+            case None       => boxes.getOrElse(step.box, Box(step.box)).removed(step.label)
+        )
+    .values
+    .foldLeft(0):
+      case (sum, box) => sum + box.focusingPower
 
   @main def run(): Unit = evaluate()
 
+  case class Box(number: Int, contents: VectorMap[String, Int] = VectorMap.empty):
+    def focusingPower: Int = this.contents.values.zipWithIndex.foldLeft(0):
+      case (sum, (focalLength, idx)) => sum + ((this.number + 1) * (idx + 1) * focalLength)
+    def updated(label: String, lens: Int): Box = this.copy(contents = contents.updated(label, lens))
+    def removed(label: String): Box = this.copy(contents = contents.removed(label))
+  end Box
+
   extension (str: String)
-    def aocHash: Int = str.foldLeft(0):
+    def aocHash: Int  = str.foldLeft(0):
       case (hash, char) if char != '\n' => ((hash + char.toInt) * 17) % 256
       case (hash, _)                    => hash
+    def toStep:  Step =
+      val (label, op) = str.span:
+        case '=' | '-' => false
+        case _         => true
+      Step(label = label, box = label.aocHash, focalLength = op.drop(1).toIntOption)
   end extension
+
+  case class Step(label: String, box: Int, focalLength: Option[Int])
 
   private lazy val parseInput: Seq[String] => Seq[String] = lines => lines.flatMap(_.split(','))
 
--- src/test/scala/days/day15/DayFifteenTest.scala
+++ src/test/scala/days/day15/DayFifteenTest.scala
@@ -2,6 +2,8 @@ package days.day15
 
 import days.BaseTest
 
+import DayFifteen.*
+
 class DayFifteenTest extends BaseTest:
 
   lazy val input: Seq[String] = Seq("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7")
@@ -9,8 +11,15 @@ class DayFifteenTest extends BaseTest:
   "Day fifteen's solution" must:
 
     "correctly calculate the hash code of 'HASH'" in:
-      import DayFifteen.aocHash
       "HASH".aocHash mustBe 52
 
     "correctly calculate the sum of the input's hashes" in:
       DayFifteen.partOne(input) mustBe 1320
+
+    "correctly parse the input to a step" in:
+      "rn=1".toStep mustBe Step(label = "rn", box = 0, focalLength = Some(1))
+      "cm-".toStep mustBe Step(label = "cm", box = 0, focalLength = None)
+      "ot=5".toStep mustBe Step(label = "ot", box = 3, focalLength = Some(5))
+
+    "correctly calculate the sum of the focusing power for all boxes" in:
+      DayFifteen.partTwo(input) mustBe 145
